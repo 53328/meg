8a041ced6849f41699c61037fb1c1dbd1358665e
Meg <2944789546@qq.com>
2026-09-25 18:19:41 +0800

    vault backup: 2026-09-25 18:19:41

--- .obsidian/plugins/obsidian-i18n-2.0.21/obsidian-i18n-2.0.21/src/views/cloud/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 云端翻译管理视图导出
- */
-export { CloudView, CLOUD_VIEW_TYPE } from './cloud-view';
-export { useCloudStore } from './cloud-store';
-export * from './types';
--- .obsidian/plugins/obsidian-i18n-2.0.21/obsidian-i18n-2.0.21/src/views/plugin_editor/components/regex/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './regex-stats-card';
-export * from './regex-table';
-export * from './regex-insert-card';
-export * from './regex-llm-card';
-export * from './regex-sidebar';
